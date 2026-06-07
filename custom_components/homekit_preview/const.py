b5dd085a3560a75dffca4a86b5aea65a0a646d70
thereprocase <128980993+thereprocase@users.noreply.github.com>
2026-06-06 22:34:53 -0400

    Add constants

---
+++ custom_components/homekit_preview/const.py
@@ -0,0 +1,9 @@
+DOMAIN = "homekit_preview"
+
+CONF_CREATE_NOTIFICATION = "create_notification"
+DEFAULT_CREATE_NOTIFICATION = True
+
+HOMEKIT_DOMAIN = "homekit"
+SCAN_SERVICE = "scan"
+
+DATA_COORDINATOR = "coordinator"
